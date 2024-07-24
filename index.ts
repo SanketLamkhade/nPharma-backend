1631aed4d65adf1668d273f05e2a929af50c0d38
Sanket Lamkhade <sanket.lamkhade@nanostuffs.com>
2024-07-24 11:42:04 +0530

    chnages

--- index.ts
+++ index.ts
@@ -1,21 +1,33 @@
 import { PrismaClient } from '@prisma/client'
+import express from 'express'
 
+const app = express();
+const port = 4000;
 const prisma = new PrismaClient()
 
-async function main() {
-    const user = await prisma.user.findUnique({
-        where:{
-            email: "Sanket@mail.com"
-        }
-    })
-    console.log(user)
-  }
-  
-  main()
-    .catch(e => {
-      console.error(e)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+app.post('/', async(req, res) => {
+  const { email, firstname, lastname, phone, password, division, empId } = req.body;
+  try {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      firstname,
+      lastname,
+      phone,
+      password,
+      division,
+      empId
+    },
+  });
+  res.status(201).json(user);
+  console.log(user)
+}catch (error) {
+  res.status(500).send('Server error');
+  console.log(error)
+}
 
+});
+
+app.listen(port, () => {
+  console.log('Server is running on http://localhost:',port);
+});
