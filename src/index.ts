12bd2b7a803d180a20be411f5ef696df1ecd6c17
Sanket Lamkhade <sanket.lamkhade@nanostuffs.com>
2024-07-30 12:23:57 +0530

    vercel.json added

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import userRoutes from './routes/routes';
 
 const app = express();
-// const port = 4000;
+const port = 4000;
 
 app.use(express.json());
 app.use('/api', userRoutes);
 
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
