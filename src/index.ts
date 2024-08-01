b76e8c29ae180a44182cd76c4bf3a3a2c3075816
shivani18k <shivani.kumbhar@nanostuffs.com>
2024-08-01 14:26:22 +0530

    changes added related to cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 const express = require('express');
 const userRoutes = require('./routes/routes');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
 
 const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "35mb" }));
+app.use(cors());
 const port = process.env.PORT || 4000;
+
+app.use(express.static("client/build"));
+
+// app.get("*", (req: Request, res: Response) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 
 app.use(express.json());
 app.use('/api', userRoutes);
