c0489b87c651ae672520c859c3879875cddda67d
shivani18k <shivani.kumbhar@nanostuffs.com>
2024-08-01 12:42:34 +0530

    chnages added related to port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/routes');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use('/api', userRoutes);
